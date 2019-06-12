c2ddb869dff5ae75d85929b9a0efdacac9cbc315
majka1474 <ryo.i.05181474@gmail.com>
2019-06-12 20:51:48 +0900

    one break

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,19 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
-    @task = Task.new 
+    @task = Task.new
   end
 
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
 
     if @task.save
       redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
@@ -23,12 +23,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
     if @task.update(task_params)
       redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
     else
@@ -38,14 +35,17 @@ class TasksController < ApplicationController
 
   # taksに@が付かないのは、viewで利用しない為
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    @task.destroy
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
 
     def task_params
       params.require(:task).permit(:name, :description)
+    end
+
+    def set_task
+      @task = current_user.tasks.find(params[:id])
     end
 end
